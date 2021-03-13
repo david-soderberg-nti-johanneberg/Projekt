69aef70d6a7e30f4ea0c7cb0bc80f1b5fdfb3549
david-soderberg-nti-johanneberg <david.soderberg@elev.ga.ntig.se>
2021-03-13 03:49:13 +0100

    Push4?
    Galet mycket strul på safari på mobilen, mycket fram å tillbaka med olika tester både med javascript å pseudoselectors, DOCK löstes det enkelt tillslut med att lägga till "ontouchstart" i html taggen...

--- gammal.js
+++ gammal.js
@@ -8,6 +8,7 @@ const image_container = document.querySelector(".imgContainer");
 var opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
 
+
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
@@ -20,17 +21,43 @@ window.addEventListener("scroll", function(event){
     var scroll2 = this.scrollY;
 })
 
+
+//Kanske ska försöka göra en array av detta//
+
+
+var distance1 = document.getElementById("card1").offsetTop
+var distance2 = document.getElementById("card2").offsetTop
+var distance3 = document.getElementById("card3").offsetTop
+var distance4 = document.getElementById("card4").offsetTop
+var distance5 = document.getElementById("card5").offsetTop
+var distance6 = document.getElementById("card6").offsetTop
+var scrolla = this.scrollY
+var acard1 = document.querySelector("card1")
+
+
 function myFunction(x) {
 if (x.matches) { 
+    console.log(distance1)
+    console.log(distance2)
+    console.log(distance3)
+    console.log(distance4)
+    console.log(distance5)
+    console.log(distance6)
+    console.log(scrolla  - distance1)
+    
 
+    document.getElementById("card1").style.color = "green";
 
         y = 0.6 + (scroll * 3 / (sectionY.top + section_height));
         } else {
         y = 0.2 + scroll / (sectionY.top + section_height);
 
 
-    }}
+        }
 
+}
+
+    
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll * speed}px)`;
@@ -48,14 +75,4 @@ if (x.matches) {
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
-});
-
-let controlActivationEvents = window.PointerEvent ? "pointerdown" : "touchstart mousedown";
-let controlDeactivationEvents = window.PointerEvent ? "pointerup pointerleave" : "touchend mouseup mouseleave";
-
-let clickableThings = '<comma separated list of selectors>';
-$(clickableThings).on(controlActivationEvents,function (e) {
-    $(this).addClass('active');
-}).on(controlDeactivationEvents, function (e) {
-    $(this).removeClass('active');
 });
