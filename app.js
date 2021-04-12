c590bf92422c19bc5049dfe9b74b38ff092e5b37
david-soderberg-nti-johanneberg <david.soderberg@elev.ga.ntig.se>
2021-04-12 23:38:11 +0200

    Allt

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
-var opacity = document.querySelectorAll(".opacity");
+let opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
 
-let header_height = header.offsetHeight;
-let section_height = section.offsetHeight;
+let headerclass = document.querySelector('.header');
+if(headerclass != null){
+header_height = header.offsetHeight;
+section_height = section.offsetHeight;
+}
 
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
@@ -20,13 +23,13 @@ window.addEventListener("scroll", function(event){
     var scroll2 = this.scrollY;
 })
 
-function myFunction(x) {
-if (x.matches) { 
+function parallax(x) {
+if (x.matches) {
 
 
         y = 0.6 + (scroll * 3 / (sectionY.top + section_height));
         } else {
-        y = 0.2 + scroll / (sectionY.top + section_height);
+        y = 0.3 + scroll / (sectionY.top + section_height);
 
 
     }}
@@ -37,9 +40,9 @@ if (x.matches) {
     })
 
       
-      var x = window.matchMedia("(max-width: 700px)")
-      myFunction(x)
-      x.addListener(myFunction) 
+      var x = window.matchMedia("(max-width: 1550px)")
+      parallax(x)
+      x.addListener(parallax) 
       
     opacity.forEach(element => {
         element.style.opacity = y
@@ -50,12 +53,100 @@ if (x.matches) {
 
 });
 
-let controlActivationEvents = window.PointerEvent ? "pointerdown" : "touchstart mousedown";
-let controlDeactivationEvents = window.PointerEvent ? "pointerup pointerleave" : "touchend mouseup mouseleave";
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const optionsList = document.querySelectorAll(".option");
 
-let clickableThings = '<comma separated list of selectors>';
-$(clickableThings).on(controlActivationEvents,function (e) {
-    $(this).addClass('active');
-}).on(controlDeactivationEvents, function (e) {
-    $(this).removeClass('active');
+
+
+if(selected != null) {
+let option1 = document.getElementById("1");
+let option2 = document.getElementById("2");
+let option3 = document.getElementById("3");
+let option4 = document.getElementById("4");
+let option5 = document.getElementById("5");
+let option6 = document.getElementById("6");
+let titleoption = document.querySelector(".titleoption");
+let infooption = document.querySelector(".infooption");
+let imgoption = document.querySelector(".imgoption")
+
+option1.addEventListener("click", () => {
+console.log(1)
+titleoption.innerHTML = "Skiing";
+infooption.innerHTML = "Insert cool information about our amazing skiing service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla";
+imgoption.src = "img/skiservice.png"
+mapEl.style.opacity = "100%";
+movecenter(60.65960245616614, 12.965496154245761)
+})
+option2.addEventListener("click", () => {
+console.log(2)
+titleoption.innerHTML = "Biking";
+infooption.innerHTML = "Insert cool information about our amazing Biking service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla"
+imgoption.src = "img/bikeservice.png"
+mapEl.style.opacity = "100%";
+movecenter(57.49697653074465, 13.109522849249215)
+})
+option3.addEventListener("click", () => {
+console.log(3)
+titleoption.innerHTML = "Hotair Balloon";
+infooption.innerHTML = "Insert cool information about our amazing Ballooning service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla"
+imgoption.src = "img/balloonservice.png"
+mapEl.style.opacity = "100%";
+movecenter(55.580678402785864, 12.931632625374906)
+})
+option4.addEventListener("click", () => {
+console.log(4)
+titleoption.innerHTML = "Scuba Diving";
+infooption.innerHTML = "Insert cool information about our amazing Scuba service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla"
+imgoption.src = "img/scubaservice.png"
+mapEl.style.opacity = "100%";
+movecenter(59.33156797179015, 18.048364543293097)
+})
+option5.addEventListener("click", () => {
+console.log(5)
+titleoption.innerHTML = "Hiking";
+infooption.innerHTML = "Insert cool information about our amazing Hiking service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla"
+imgoption.src = "img/hikingservice.png"
+mapEl.style.opacity = "100%";
+movecenter(59.33819824334666, 17.913416242454463)
+})
+option6.addEventListener("click", () => {
+console.log(6)
+titleoption.innerHTML = "Climbing";
+infooption.innerHTML = "Insert cool information about our amazing Climbing service.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas lectus libero, sit amet semper lectus vulputate malesuada. Maecenas imperdiet nulla augue, at condimentum magna pretium vitae. Sed bibendum imperdiet arcu, sit amet dictum orci blandit eget. Nam eget fringilla sem. Praesent vel euismod enim. Donec dapibus pharetra fermentum. Nam orci ligula, pulvinar sed mauris eu, tincidunt porta sem. Pellentesque non neque dictum, congue risus ut, aliquet nunc <br><br> 1. Lorem <br><br> 2.Bla bla <br><br> 3.Bla bla"
+imgoption.src = "img/climbingservice.png"
+mapEl.style.opacity = "100%";
+movecenter(59.29911868495861, 17.99155767646861)
 });
+
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+
+
+})};
+
+var googlemap
+var marker
+function initMap() {
+  var uluru = { lat: 57.690518503367244, lng: 11.974776379023586 };
+  googlemap = new google.maps.Map(document.getElementById("mapEl"), {
+    zoom: 17,
+    center: uluru,
+  });
+  marker = new google.maps.Marker({
+    position: uluru,
+    map: googlemap,
+  });
+}
+function movecenter(lati,long) {
+  googlemap.panTo({lat: Number(lati), lng: Number(long)});
+  marker.setPosition({lat: Number(lati), lng: Number(long)})
+}
